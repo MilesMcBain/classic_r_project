8c5b4dc2a2480deffaae14f8f44c795b4d7a0aa7
Miles McBain <miles.mcbain@gmail.com>
2024-06-27 15:11:12 +1000

    fit_fold_calc_results in own file

---
+++ R/fit_fold_calc_results.R
@@ -0,0 +1,38 @@
+fit_fold_calc_results <- function(split, num_trees, mtry) {
+  model <-
+    randomForest(
+      scientificName ~ .,
+      data = training(split),
+      mtry = mtry,
+      ntree = num_trees
+    )
+  test_set <-
+    testing(split) |>
+    mutate(
+      is_moluccus = if_else(
+        scientificName == "Threskiornis moluccus",
+        1,
+        0
+      )
+    )
+  test_set$predicted_prob_is_moluccus <-
+    predict(
+      model,
+      newdata = test_set,
+      type = "prob"
+    ) |>
+    _[, 1] # We get a matrix with 2 columns, 1 per class. Threskiornis moluccus is column 1.
+
+  roc_object <- roc(
+    test_set$is_moluccus,
+    test_set$predicted_prob_is_moluccus
+  )
+
+  # use auc and accuracy as our summary statistics
+  data.frame(
+    auc = auc(roc_object) |> as.numeric(),
+    accuracy = sum(test_set$is_moluccus > 0.5 & test_set$is_moluccus == 1) / nrow(test_set),
+    num_trees = num_trees,
+    mtry = mtry
+  )
+}
